00ee62f777b82c87733ce100981ba75e1990b6f5
罗晓俊 <luoxiaojun1992@sina.cn>
2019-11-02 13:25:55 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,4 +8,4 @@ service impala-catalog start
 service impala-server start
 service kudu-master start
 service kudu-tserver start
-tail -f /var/log/kudu/kudu-master.INFO -f /var/log/kudu/kudu-tserver.INFO -f /var/log/impala/impalad.INFO -f /var/log/impala/catalogd.INFO -f /var/log/impala/statestored.INFO
+tail -f /var/log/impala/impalad.INFO -f /var/log/impala/catalogd.INFO -f /var/log/impala/statestored.INFO
