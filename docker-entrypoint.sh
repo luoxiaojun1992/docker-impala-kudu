d3f69706416ab60a6379067dc884c748864ed757
罗晓俊 <luoxiaojun1992@sina.cn>
2019-11-02 13:26:19 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,4 @@ service hive-metastore start
 service impala-state-store start
 service impala-catalog start
 service impala-server start
-service kudu-master start
-service kudu-tserver start
 tail -f /var/log/impala/impalad.INFO -f /var/log/impala/catalogd.INFO -f /var/log/impala/statestored.INFO
